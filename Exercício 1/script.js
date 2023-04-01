0db2b8a78621a5b4b1cdd92e61f30403d26ea134
marciagfsantos <marciagfs95@gmail.com>
2023-04-01 16:16:45 +0100

    first commit

---
+++ Exercício 1/script.js
@@ -0,0 +1,16 @@
+//Você deverá criar uma variável de cada tipo primitivo:
+
+// Variável do Tipo String
+var serieName ='Friends'
+
+//Variável do Tipo Number
+var season=2
+
+var episode=14
+
+//Variável do Tipo Boolean
+var isThebestseriesintheworld = true
+
+
+console.log(' Eu estou a assistir o episódio ' + episode + ' da temporada ' + season + ' da série ' + serieName + '. Friends é a melhor série do mundo, não é? ')
+
